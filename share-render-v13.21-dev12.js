e1ffea9e2496da8c08ab8209047ce201088683c1
thomaslee78-beep <thomaslee78@gmail.com>
2026-08-29 20:13:34 -0700

    Bump Shirt Guide Phase 1E preview cache

--- share-render-v13.21-dev12.js
+++ share-render-v13.21-dev12.js
@@ -7,7 +7,7 @@
   'use strict';
 
   const HOTFIX='13.23.6.3-photo-fixes';
-  const BG_PREVIEW='13.23-bg-phase1d';
+  const BG_PREVIEW='13.23-bg-phase1e';
   document.write('<script src="share-render-v13.21-dev12-core.js?v='+HOTFIX+'"><\/script>');
 
   const modules=[
